71fb211b1edb76602f0f3065b603e9c44c86bc71
talidoom <talidoom@yandex.ru>
2023-11-29 15:07:43 +0300

    build

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -17,14 +17,14 @@ class Api {
         headers: this._headers,
       }).then(this._checkResponse);
     }
-  
+
     getUserInfo() {
       return fetch(`${this._baseUrl}/users/me`, {
         method: 'GET',
         headers: this._headers,
       }).then(this._checkResponse);
     }
-  
+
     setUserInfo(name, status) {
       return fetch(`${this._baseUrl}/users/me`, {
         method: 'PATCH',
@@ -35,7 +35,7 @@ class Api {
         }),
       }).then(this._checkResponse);
     }
-  
+
     createCard(name, link) {
       return fetch(`${this._baseUrl}/cards`, {
         method: 'POST',
@@ -46,21 +46,21 @@ class Api {
         }),
       }).then(this._checkResponse);
     }
-  
+
     deleteCard(id) {
       return fetch(`${this._baseUrl}/cards/${id}`, {
         method: 'DELETE',
         headers: this._headers,
       }).then(this._checkResponse);
     }
-  
+
     toggleLike(id, like) {
       return fetch(`${this._baseUrl}/cards/likes/${id}`, {
         method: like ? 'DELETE' : 'PUT',
         headers: this._headers,
       }).then(this._checkResponse);
     }
-  
+
     setUserAvatar(avatar) {
       return fetch(`${this._baseUrl}/users/me/avatar`, {
         method: 'PATCH',
@@ -71,13 +71,13 @@ class Api {
       }).then(this._checkResponse);
     }
   }
-  
+
   const api = new Api({
-    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-61',
+    baseUrl: 'https://localhost/api',
     headers: {
       authorization: '246e7422-483c-4e41-82f4-4aaaa5291029',
       'Content-Type': 'application/json',
     },
   });
-  
+
   export default api;
